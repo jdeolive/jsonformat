6328fa4e084987d1ceb7571d0773b09ce4f54418
jdeolive <jdeolive@opengeo.org>
2013-06-05 12:00:04 -0600

    reading property from stdin or argv

--- jsonformat.py
+++ jsonformat.py
@@ -2,6 +2,7 @@
 
 import json, sys, collections
 
+data = open(sys.argv[1]).read() if len(sys.argv) > 1 else sys.stdin.read()
 d = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
-print json.dumps(d.decode(sys.argv[1]), indent=2)
+print json.dumps(d.decode(data), indent=2)
 
