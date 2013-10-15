c54598b0b42f73410e7764e83af54856ecf97e11
jdeolive <jdeolive@boundlessgeo.com>
2013-10-15 16:12:03 -0600

    updating forumla to v1.1

--- jsonformat.rb
+++ jsonformat.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jsonformat < Formula
   homepage 'https://github.com/jdeolive/jsonformat'
-  url 'https://github.com/jdeolive/jsonformat/archive/v1.0.tar.gz'
-  sha1 '9335328aecfb0fecd973ebf86aa807162e1db8c0'
+  url 'https://github.com/jdeolive/jsonformat/archive/v1.1.tar.gz'
+  sha1 'b7ab521cc81f5c538bcd39873fa9d8ff98aa6dde'
 
   def install
     bin.install "jsonformat.py" => "jsonformat"
