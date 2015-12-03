c8ae89b6e5bef89e369b1d1906bc98006aff8143
jdeolive <jdeolive@gmail.com>
2015-12-03 08:33:23 -0700

    Added some options to format script.
    - inplace file formatting
    - unformatting option

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 A simple Python script for parsing and formatting a JSON string.
 
-    python jsonformat.py '{"foo":"bar", "baz": 2}'
+    # format from stdin
+    % echo '{"foo":"bar", "baz": 2}' | python jsonformat.py 
+
+    # format from file
+    % python jsonformat.py /path/to/file.json
+
+    # format file inplace
+    % python jsonformat.py -i /path/to/file.json
+
+    # unformat
+    % python jsonformat.py -u /path/to/file.json
--- jsonformat.py
+++ jsonformat.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 
-import json, sys, collections
+import json, sys, collections, argparse
 
-data = open(sys.argv[1]).read() if len(sys.argv) > 1 else sys.stdin.read()
-d = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
-print json.dumps(d.decode(data), indent=2)
+p = argparse.ArgumentParser(description='JSON Formatter')
+p.add_argument('-f', '--filename', help='File to format')
+p.add_argument('-i', '--inplace', action='store_true', help='Format file in place')
+p.add_argument('-u', '--unformat', action='store_true', help='Unformat the JSON')
+
+args = p.parse_args()
+
+file_name = args.filename if args.filename else None
+if file_name:
+    f = open(file_name)
+    data = f.read()
+    f.close()
+else:
+    data = sys.stdin.read()
+
+decoder = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
+
+format_args = {}
+if not args.unformat:
+    format_args['indent'] = 2
+formatted = json.dumps(decoder.decode(data), **format_args)
+
+if file_name and args.inplace:
+    f = open(file_name, 'w')
+    f.write(formatted)
+    f.close()
+else:
+    print(formatted)
 
